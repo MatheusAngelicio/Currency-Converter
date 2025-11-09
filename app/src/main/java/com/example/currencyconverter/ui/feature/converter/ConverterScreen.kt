a31654ad82c3b7f2c72c5c67c382d7fb52f11327
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-09 07:36:04 -0300

    feat: Add converter screen and icons
    Adds the initial boilerplate for the currency converter screen, including a composable function and its preview.
    
    Also adds two new vector drawables:
    - `ic_arrow_drop_down`
    - `ic_arrow_downward`

---
+++ app/src/main/java/com/example/currencyconverter/ui/feature/converter/ConverterScreen.kt
@@ -0,0 +1,24 @@
+package com.example.currencyconverter.ui.feature.converter
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
+
+@Composable
+fun ConverterScreen() {
+
+}
+
+@Composable
+fun ConverterContent() {
+
+}
+
+@Preview
+@Composable
+private fun ConverterContentPreview() {
+    CurrencyConverterTheme {
+        ConverterContent()
+    }
+}
