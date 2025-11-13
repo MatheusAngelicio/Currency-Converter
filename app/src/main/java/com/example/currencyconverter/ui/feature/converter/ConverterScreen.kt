14cf9973be89dd96bc97da7eaeb89bcaf0b19bf7
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-13 17:21:08 -0300

    feat: Add top app bar to converter screen
    Adds a `Scaffold` with a `CenterAlignedTopAppBar` to the `ConverterContent` composable. The app bar displays the title "Currency Converter".

--- app/src/main/java/com/example/currencyconverter/ui/feature/converter/ConverterScreen.kt
+++ app/src/main/java/com/example/currencyconverter/ui/feature/converter/ConverterScreen.kt
@@ -1,7 +1,10 @@
 package com.example.currencyconverter.ui.feature.converter
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.currencyconverter.ui.theme.CurrencyConverterTheme
 
@@ -10,8 +13,16 @@ fun ConverterScreen() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConverterContent() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Currency Converter") }
+            )
+        }
+    ) { }
 
 }
 
