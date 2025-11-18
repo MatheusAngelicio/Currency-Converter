87030e2434fd1b8c352998c71d208b7caa409e91
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-18 06:14:29 -0300

    feat: Add repository for currency conversion
    Adds `CurrencyRepository` interface in the domain layer and its implementation, `CurrencyRepositoryImpl`, in the data layer.
    
    The implementation uses a Ktor client to perform the currency conversion and maps the response to a `CurrencyConversion` domain model. Error handling is managed using `runCatching` to return a `Result`.

---
+++ app/src/main/java/com/example/currencyconverter/data/repository/CurrencyRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.currencyconverter.data.repository
+
+import com.example.currencyconverter.data.network.KtorClient
+import com.example.currencyconverter.domain.model.CurrencyConversion
+import com.example.currencyconverter.domain.repository.CurrencyRepository
+
+class CurrencyRepositoryImpl(
+    private val ktorClient: KtorClient
+) : CurrencyRepository {
+
+    override suspend fun convertCurrency(
+        fromCurrency: String,
+        toCurrency: String,
+        amount: Double
+    ): Result<CurrencyConversion> {
+        // "runCatching" > serve pra trabalhar com "Result" :
+        // vai rodar o codigo que esta dentro {}
+        // se extourar alguma excecao o runCatching vai retornar um Result com a excecao
+        // se cair no sucesso vai retornar o Result com sucesso
+        return runCatching {
+            val response = ktorClient.convertCurrency(
+                fromCurrency = fromCurrency,
+                toCurrency = toCurrency,
+                amount = amount
+            )
+
+            // retorna um objeto CurrencyConversion ja mapeado com os dados da response
+            CurrencyConversion(
+                baseCode = response.baseCode,
+                targetCode = response.targetCode,
+                conversionRate = response.conversionRate.toString(),
+                conversionResult = response.conversionResult.toString()
+            )
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/currencyconverter/domain/repository/CurrencyRepository.kt
@@ -0,0 +1,13 @@
+package com.example.currencyconverter.domain.repository
+
+import com.example.currencyconverter.domain.model.CurrencyConversion
+
+interface CurrencyRepository {
+
+    suspend fun convertCurrency(
+        fromCurrency: String,
+        toCurrency: String,
+        amount: Double
+    ): Result<CurrencyConversion>
+
+}
