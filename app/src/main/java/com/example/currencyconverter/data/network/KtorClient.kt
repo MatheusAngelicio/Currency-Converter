30723f16905c3362863f1769463b25779ef6a2b7
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-18 05:56:37 -0300

    feat: Add currency conversion API call

--- app/src/main/java/com/example/currencyconverter/data/network/KtorClient.kt
+++ app/src/main/java/com/example/currencyconverter/data/network/KtorClient.kt
@@ -1,12 +1,15 @@
 package com.example.currencyconverter.data.network
 
 import com.example.currencyconverter.data.network.apiKey.CURRENCY_API_KEY
+import com.example.currencyconverter.data.network.model.CurrencyConversionResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.client.request.get
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -32,4 +35,17 @@ object ApiClient {
             logger = Logger.SIMPLE
         }
     }
+}
+
+class KtorClient(private val client: HttpClient = ApiClient.client) {
+
+    suspend fun convertCurrency(
+        fromCurrency: String,
+        toCurrency: String,
+        amount: Double
+    ): CurrencyConversionResponse {
+        return client.get("pair/${fromCurrency}/${toCurrency}/${amount}").body()
+    }
+
+
 }
